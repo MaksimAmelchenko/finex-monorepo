f6efce0b56fbf9a74482f99aa5badd5263cfff0d
Maksim Amelchenko <m.amelchenko@gmail.com>
2021-04-09 11:04:08 +0200

    fix: use old auth links (to prevent the change of the frontend structure)

--- packages/api-service/services/reset-password-request/methods/get-reset-password-confirmation-url.ts
+++ packages/api-service/services/reset-password-request/methods/get-reset-password-confirmation-url.ts
@@ -3,5 +3,6 @@ import config from '../../../libs/config';
 const appDomain = config.get('appDomain');
 
 export function getResetPasswordConfirmationUrl(token: string): string {
-  return `https://${appDomain}/reset-password/${token}/confirm`;
+  // return `https://${appDomain}/reset-password/${token}/confirm`;
+  return `https://${appDomain}/password_recovery/confirm?token=${token}`;
 }
