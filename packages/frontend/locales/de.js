0cb1e55bd613c996d5c93b823713c65461c91874
Maksim Amelchenko <m.amelchenko@gmail.com>
2022-01-31 18:22:43 +0100

    feat(frontend): add i18n support

---
+++ packages/frontend/locales/de.js
@@ -0,0 +1,19 @@
+module.exports = {
+  Error: {
+    'The requested resource is restricted and requires authentication_':
+      'The requested resource is restricted and requires authentication.',
+    'Could not connect to servers, please try again': 'Could not connect to servers, please try again',
+    'Something wrong happened with server, please try again_':
+      'Something wrong happened with server, please try again.',
+    'Something went wrong, please try again': 'Something went wrong, please try again',
+  },
+  SignInForm: {
+    'Enter username / e-mail address': 'Enter username / e-mail address',
+    'Invalid e-amil address': 'Invalid e-amil address',
+    'Enter a password': 'Enter a password',
+    'Invalid username or password': 'Invalid username or password',
+    Username: 'Username',
+    Password: 'Password',
+    'Sign In': 'Sign In',
+  },
+};
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -6,13 +6,94 @@ import { App } from './app/app';
 import { createMainContext } from './app/core/main-context';
 import { initializeMainStore } from './app/core/initialize-stores';
 
-createMainContext(initializeMainStore());
+import * as ru from '../locales/ru.js';
+import { initializeI18n } from './app/lib/core/i18n';
 
-render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-  document.getElementById('root')
-);
+const languages = ['ru', 'en', 'de'];
+// const lang = window.location.pathname.split('/')[1];
+const lang = languages[0];
+const defaultLanguage = languages[0];
+const currentLocale = languages.includes(lang) ? lang : defaultLanguage;
+
+async function initI18n(): Promise<void> {
+  switch (currentLocale) {
+    case 'ru': {
+      initializeI18n(
+        {
+          ru: {
+            ...ru,
+            date: {
+              week: { short: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'] },
+              formats: {
+                short: 'd MMM yyyy',
+              },
+            },
+            time: {
+              format: 'HH:mm',
+            },
+          },
+        },
+        currentLocale,
+        defaultLanguage
+      );
+      break;
+    }
+    case 'en': {
+      const en = await import(/* webpackChunkName: "locale-en" */ '../locales/en.js');
+      initializeI18n(
+        {
+          en: {
+            ...en,
+            date: {
+              week: { short: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] },
+              formats: {
+                short: 'd MMM yyyy',
+              },
+            },
+            time: {
+              format: 'HH:mm',
+            },
+          },
+        },
+        currentLocale,
+        defaultLanguage
+      );
+      break;
+    }
+    case 'de': {
+      const de = await import(/* webpackChunkName: "locale-de" */ '../locales/de.js');
+      initializeI18n(
+        {
+          en: {
+            ...de,
+            date: {
+              week: { short: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] },
+              formats: {
+                short: 'd MMM yyyy',
+              },
+            },
+            time: {
+              format: 'HH:mm',
+            },
+          },
+        },
+        currentLocale,
+        defaultLanguage
+      );
+      break;
+    }
+  }
+}
+
+initI18n().then(() => {
+  createMainContext(initializeMainStore());
+
+  render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+    document.getElementById('root')
+  );
+});
