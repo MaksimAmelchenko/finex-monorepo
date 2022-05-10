56a641a8f34d9ea4b01ccd262e1a1906e116d705
Maksim Amelchenko <m.amelchenko@gmail.com>
2022-05-10 12:27:07 +0200

    fix: IconButton component

--- packages/ui-kit/src/lib/icon-button/icon-button.tsx
+++ packages/ui-kit/src/lib/icon-button/icon-button.tsx
@@ -1,32 +1,24 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 
-// import { Link } from '../Link/Link';
 import styles from './icon-button.module.scss';
 
 export interface IconButtonProps extends Omit<HTMLAttributes<HTMLButtonElement | HTMLAnchorElement>, 'css'> {
   children: React.ReactNode; //  The content of the component.
   className?: string; // Override or extend the styles applied to the component.
-  // color?: 'blue' | 'orange';
   disabled?: boolean; // If true, the component is disabled.
   loading?: boolean; // If true, the component is disabled.
   href?: string; // The URL to link to when the button is clicked. If defined, an a element will be used as the root node.
   size?: 'small' | 'medium' | 'large'; // The size of the component.
-  // variant?: 'contained';
-  // type?: 'button' | 'submit';
 }
 
-export const IconButton = ({ children, ...rest }: IconButtonProps) => {
+export const IconButton = forwardRef(({ children, ...rest }: IconButtonProps, ref) => {
   return <ButtonStyledInner {...rest}>{children}</ButtonStyledInner>;
-};
+});
 
 const ButtonStyledInner = ({
-  // type = 'button',
   size = 'medium',
-  // variant = 'contained',
-  // color = 'orange',
-  // fullSize = false,
   loading = false,
   disabled = false,
   className,
@@ -36,9 +28,6 @@ const ButtonStyledInner = ({
   const classNameStyles = clsx(styles.button, className, {
     [styles[`button_size_${size}`]]: size,
     [styles[`button_disabled`]]: loading || disabled,
-    // [styles[`button_variant_${variant}`]]: true,
-    // [styles[`button_color_${color}`]]: true,
-    // [styles['button_fullSize']]: fullSize,
   });
 
   return href ? (
