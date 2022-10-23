9a2f5c049914a4ec9906d556152324c9ccefa282
Maksim Amelchenko <m.amelchenko@gmail.com>
2022-10-23 12:28:34 +0200

    feat(ui-kit): add danger button color

--- packages/ui-kit/src/lib/button/button.tsx
+++ packages/ui-kit/src/lib/button/button.tsx
@@ -8,12 +8,12 @@ import styles from './button.module.scss';
 export interface IButtonProps extends Omit<HTMLAttributes<HTMLButtonElement | HTMLAnchorElement>, 'css'> {
   children?: React.ReactNode; //  The content of the component.
   className?: string; // Override or extend the styles applied to the component.
-  color?: 'primary' | 'secondary';
+  color?: 'primary' | 'secondary' | 'danger';
   disabled?: boolean; // If true, the component is disabled.
   loading?: boolean; // If true, the component is disabled.
   fullSize?: boolean; // If true, the button will take up the full width of its container
   href?: string; // The URL to link to when the button is clicked. If defined, an a element will be used as the root node.
-  size?: 'small'| 'medium' | 'large'; // The size of the component.
+  size?: 'small' | 'medium' | 'large'; // The size of the component.
   variant?: 'contained' | 'outlined';
   type?: 'button' | 'submit';
 }
