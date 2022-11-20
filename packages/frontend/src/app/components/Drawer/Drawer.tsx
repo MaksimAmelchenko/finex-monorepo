163cef0e6bdf64f13242cf89908d99198ce636fe
Maksim Amelchenko <m.amelchenko@gmail.com>
2022-11-20 18:35:48 +0100

    fix: remove useless adjustment of z-index in Drawer

--- packages/frontend/src/app/components/Drawer/Drawer.tsx
+++ packages/frontend/src/app/components/Drawer/Drawer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import MuiDrawer from '@mui/material/Drawer';
 import clsx from 'clsx';
-import { useTheme } from '@mui/material/styles';
 
 import styles from './Drawer.module.scss';
 
@@ -12,18 +11,8 @@ interface IDrawerProps {
 }
 
 export function Drawer({ isOpened, children, className }: IDrawerProps): JSX.Element {
-  const theme = useTheme();
-
   return (
-    <MuiDrawer
-      anchor="right"
-      disableEscapeKeyDown={true}
-      open={isOpened}
-      sx={{
-        // because the header has "drawer + 1" to override the left menu
-        zIndex: theme.zIndex.drawer + 2,
-      }}
-    >
+    <MuiDrawer anchor="right" disableEscapeKeyDown={true} open={isOpened}>
       <div className={clsx(styles.container, className)}>{isOpened && <>{children}</>}</div>
     </MuiDrawer>
   );
