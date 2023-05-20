df3327cefa0166f02a3d8d8bb5f49e51cd917c1d
Maksim Amelchenko <m.amelchenko@gmail.com>
2023-05-20 17:54:41 +0200

    fix: GA initialization

--- packages/frontend/src/app/lib/analytics.ts
+++ packages/frontend/src/app/lib/analytics.ts
@@ -26,9 +26,10 @@ interface IOptions {
 
 function addGoogleAnalytics({ trackingId }: IOptions) {
   return new Promise((resolve, reject) => {
-    if (window.GDPRCookiesGoogleAnalyticsAdded) return resolve(true);
+    if (window.GDPRCookiesGoogleAnalyticsAdded) {
+      return resolve(true);
+    }
 
-    const head = document.getElementsByTagName('head')[0];
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.onload = () => {
@@ -36,29 +37,20 @@ function addGoogleAnalytics({ trackingId }: IOptions) {
       resolve(true);
     };
     script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`;
-    head.appendChild(script);
-
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function (...args: any[]) {
-      window.dataLayer.push(args);
-    };
-    window.gtag('js', new Date());
-
-    window.gtag('config', trackingId, {});
+    script.async = true;
+    document.head.appendChild(script);
   });
 }
 
 function initializeGoogleAnalytics({ trackingId }: IOptions) {
   if (!window.GDPRCookiesGoogleAnalyticsInitialized) {
-    /*
     window.dataLayer = window.dataLayer || [];
-    window.gtag = function (...args: any[]) {
-      window.dataLayer.push(args);
+    window.gtag = function () {
+      // eslint-disable-next-line prefer-rest-params
+      window.dataLayer.push(arguments);
     };
     window.gtag('js', new Date());
-
-    window.gtag('config', trackingId, {});
-    */
+    window.gtag('config', trackingId);
 
     window.GDPRCookiesGoogleAnalyticsInitialized = true;
   }
